187acfdaf99d76bfcd4e8850764039c3c2f16f21
QuasimKhan <quasimkhanpersonal@gmail.com>
2025-02-07 15:58:40 +0530

    Updated

--- app/api/user/[userId]/route.ts
+++ app/api/user/[userId]/route.ts
@@ -1,15 +1,19 @@
 import { NextResponse, NextRequest } from "next/server";
 import { connectToDatabase } from "@/lib/db";
-import User from "@/models/User";
+import User, { IUser } from "@/models/User";
 import bcrypt from "bcryptjs";
 
 
 
 
-export async function GET(req: NextRequest, { params }: { params: { userId: string } }) {
+
+export async function GET(
+  req: NextRequest,
+  context: { params: { userId: string } },
+): Promise<NextResponse<IUser | { error: string }>> {
   try {
     await connectToDatabase();
-    const user = await User.findById(params.userId);
+    const user = await User.findById<IUser>(context.params.userId);
     if (!user) return NextResponse.json({ error: "User not found" }, { status: 404 });
 
     return NextResponse.json(user);
